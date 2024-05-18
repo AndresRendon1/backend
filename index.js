7f214301d5d171670af6a825e0bf65cda9d1f636
AndresRendon1 <70673672+AndresRendon1@users.noreply.github.com>
2024-05-18 17:26:34 -0500

    correcciones

--- index.js
+++ index.js
@@ -1,53 +1,22 @@
-// pages/index.js
-import axios from 'axios';
-import { useState } from 'react';
+// index.js
 
-export default function Home() {
-  const [heroName, setHeroName] = useState('');
-  const [heroData, setHeroData] = useState(null);
-  const [error, setError] = useState(null);
+const express = require('express');
+const fetch = require('node-fetch');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+const port = 3000;
 
-  const fetchHeroData = async () => {
-    try {
-      setError(null); // Reset error before fetching data
-      const response = await axios.get(`http://localhost:8080/api/superhero?hero=${heroName}`);
-      setHeroData(response.data);
-    } catch (error) {
-      console.error('Error fetching hero data:', error);
-      setHeroData(null);
-      setError('Error fetching hero data. Please try again.');
-    }
-  };
+// Middleware para servir archivos estáticos
+app.use(express.static('public'));
 
-  return (
-    <div className="container">
-      <h1>Superhero Search</h1>
-      <input
-        type="text"
-        value={heroName}
-        onChange={(e) => setHeroName(e.target.value)}
-        placeholder="Enter superhero name"
-      />
-      <button onClick={fetchHeroData}>Search</button>
+// Middleware para redirigir las solicitudes al servidor de API en el puerto 8080
+app.use('/api', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+// Página principal
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
-      {heroData && (
-        <div>
-          <h2>{heroData.name}</h2>
-          <p>Full Name: {heroData.biography.fullName}</p>
-          <h3>Powerstats:</h3>
-          <ul>
-            <li>Intelligence: {heroData.powerstats.intelligence}</li>
-            <li>Strength: {heroData.powerstats.strength}</li>
-            <li>Speed: {heroData.powerstats.speed}</li>
-            <li>Durability: {heroData.powerstats.durability}</li>
-            <li>Power: {heroData.powerstats.power}</li>
-            <li>Combat: {heroData.powerstats.combat}</li>
-          </ul>
-          <img src={heroData.images.md} alt={heroData.name} />
-        </div>
-      )}
-    </div>
-  );
-}
+app.listen(port, () => {
+    console.log(`Frontend server listening on port ${port}`);
+});
